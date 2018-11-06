b18996957fde0be158999bb97f682b4cba9aa3d2
Yifei Feng <1192265+yifeif@users.noreply.github.com>
2018-11-05 16:59:48 -0800

    Relax tf version requirement.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools.dist import Distribution
 
 __version__ = '0.0.1'
 REQUIRED_PACKAGES = [
-    'tensorflow == 1.11.0',
+    'tensorflow >= 1.12.0',
 ]
 project_name = 'tensorflow-zero-out'
 
